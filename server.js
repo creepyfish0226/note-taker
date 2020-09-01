f1c7ebc11f4f9910d296c8b61ba4885394f21c61
Jeremy Gager <jeremymgager@aol.com>
2020-09-01 16:44:04 -0400

    adjusted the db file

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Dependencies
 var express = require("express");
 var path =  require("path")
+var fs = require("fs")
 var app = express();
 var PORT = 3000;
 var db = require("./db/db.json")
@@ -10,12 +11,33 @@ app.use(express.json());
 
 app.use(express.static('public'))
 
+function writeDb (){
+  fs.writeFile("./db/db.json", JSON.stringify(db), (err) => {
+    if (err) throw err;
+    console.log('The file has been saved!');
+  });
+}
+
+
 app.get('/notes', function (req, res) {
   res.sendFile(path.join(__dirname,"./public/notes.html"))
 })
 
 app.get('/api/notes', function (req, res) {
   res.json(db)
+})
+
+app.post('/api/notes', function (req, res){
+  console.log(req.body)
+  if(db.length){
+    req.body.id = db[db.length-1].id+1
+  }else{
+    req.body.id = 1
+  }
+  db.push(req.body)
+  writeDb()
+  console.log(db)
+  res.sendStatus(200)
 })
 
 app.get('*', function (req, res) {
